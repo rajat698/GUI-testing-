9eaba2626248780c4db71788d479131338bb5ad9
rajat698 <67538489+rajat698@users.noreply.github.com>
2023-04-07 11:27:36 -0700

    More tests addded

--- tests/test_first.py
+++ tests/test_first.py
@@ -1,6 +1,8 @@
 from playwright.sync_api import Page, expect
+import re
 
 website = 'http://34.171.206.146:3200'
+url = website[7:]
 
 #Test to check if the website is responsive
 def test_response(page: Page):
@@ -61,14 +63,64 @@ def test_GoBackButton_size(page: Page):
     assert size_details['height'] == 48
     assert size_details['width'] == 300
 
-
+#Test to check the SIZE of the text box on the More Info page
 def test_textBox_size(page):
     page.goto(website)
     page.get_by_role("button", name="More Info").click()
-    page.get_by_placeholder("Say Something Random").click()
-    text_box = page.get_by_role("button", name="Go Back")
+    text_box = page.get_by_placeholder("Say Something Random")
     size_details = text_box.bounding_box()
 
     #Height and width calculated using the bounding_box method
-    assert size_details['height'] == 48
-    assert size_details['width'] == 300
+    assert size_details['height'] == 50
+    assert size_details['width'] == 205.5
+
+#Test to check the location of the radio button on the home page
+def test_radioButton_location(page: Page):
+    page.goto(website)
+    checkBox = page.get_by_label("Hello and welcome to grading my assignment")
+    location_details = checkBox.bounding_box()
+    
+    #Location calculated using bounding_box method
+    assert location_details['x'] == 545
+    assert location_details['y'] == 521.0625
+
+#Test to check the location of the main image on the sign in page
+def test_image_location(page: Page):
+    page.goto(website)
+    page.get_by_role("button", name="Sign In").click()
+    image = page.get_by_role("img")
+    location_details = image.bounding_box()
+
+    #Location calculated using bounding_box method
+    assert location_details['x'] == 490
+    assert location_details['y'] == 116
+
+#Test to check the location of the 'Last Name' text field on the sign up page
+def test_textBox_location(page:Page):
+    page.goto(website)
+    page.get_by_role("button", name="Sign Up").click()
+    text_field = page.get_by_placeholder("Last Name")
+    location_details = text_field.bounding_box()
+
+    #Location calculated using bounding_box method
+    assert location_details['x'] == 641.7578125
+    assert location_details['y'] == 212
+
+# Test to check the flow of pages main -> Sign In
+def test_flow_to_sign_in(page: Page):
+    page.goto(website)
+    page.get_by_role("button", name="Sign In").click()
+    expect(page).to_have_url(re.compile(f"{url}/signin"))
+
+#Test to check the flow of pages main -> Sign Up
+def test_flow_to_sign_up(page: Page):
+    page.goto(website)
+    page.get_by_role("button", name="Sign Up").click()
+    expect(page).to_have_url(re.compile(f"{url}/signup"))
+
+#Test to check the flow of pages main -> More Info - > Go Back
+def test_flow_to_more_info(page: Page):
+    page.goto(website)
+    page.get_by_role("button", name="More Info").click()
+    page.get_by_role("button", name="Go Back").click()
+    expect(page).to_have_url(re.compile(f"{url}"))
